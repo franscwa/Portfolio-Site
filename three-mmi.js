8d46bcd4bfa5d4a79dc4a26939e132f545ee3fc0
franscwa <g.francisco@knights.ucf.edu>
2022-06-04 19:53:27 -0400

    fixed import issue

--- three-mmi.js
+++ three-mmi.js
@@ -142,15 +142,15 @@
 		</body>
 	</html>
 */
-
-class MouseMeshInteractionHandler {
+import * as THREE from 'three';
+ class MouseMeshInteractionHandler {
 	constructor(mesh_name, handler_function) {
 		this.mesh_name = mesh_name;
 		this.handler_function = handler_function;
 	}
 }
 
-class MouseMeshInteraction {
+export default class MouseMeshInteraction {
 	constructor(scene, camera) {
 		this.scene = scene;
 		this.camera = camera;
